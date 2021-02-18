5d00992f91a90ec41c7a79d51c71efcea5179dab
mrpra <mrpra@PRASHANT-LAPI>
2021-02-18 20:06:27 +0530

    Upload all file for spring data rest application

---
+++ SpringDataRest/src/main/java/com/prashant/demo/AlienRepo.java
@@ -0,0 +1,11 @@
+package com.prashant.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.prashant.demo.model.Alien;
+
+@RepositoryRestResource(collectionResourceRel="aliens",path="aliens")
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
+
+}
---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(1,'Prashant','Java');
+insert into alien values(2,'Manish','dotNet');
+insert into alien values(3,'Prasad','Python');
+insert into alien values(4,'Anil','html');
+insert into alien values(5,'Sunil','vb');
